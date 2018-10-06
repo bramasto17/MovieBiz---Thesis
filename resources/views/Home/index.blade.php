6f23d31b54fc37d3bf101ab56302c393294bfa42
ganuzaz <Ganuzaz@gmail.com>
2018-10-06 16:02:07 +0700

    fix width

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -44,17 +44,15 @@ Home
         <div class="row">
             <div class="col-xs-12 col-sm-9 gallery-slider">
                 <div class="gallery-slide">
-                    @if (!empty($history))
-                        @foreach($history as $data)
-                        <div class="item">      
-                            <a href="/movie/{{$data->id}}"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2{{$data->poster_path}}" alt=""></a>
-                        </div>
-                        @endforeach
-                    @endif
+                    @foreach($history as $data)
+                    <div class="item">
+                        <a href="/movie/{{$data->id}}"><img src="https://image.tmdb.org/t/p/w185_and_h278_bestv2{{$data->poster_path}}" alt=""></a>
+                    </div>
+                    @endforeach
                 </div>
             </div>
             <div class="col-xs-12 col-sm-3">
-                <h1>Recently Watched</h1>
+                <h1>Recent Movies</h1>
             </div>
         </div>
     </div>
