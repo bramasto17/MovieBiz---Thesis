76d222e01ee31adb8873c55e59d9e4a40c4a60f9
ganuzaz <Ganuzaz@gmail.com>
2018-10-21 17:24:38 +0700

    update : -responsive button.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,32 +6,48 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;	
 use App\User;
 use Carbon\Carbon;
+use Auth;
 class ProfileController extends Controller
 {
     //
+    public function checkFollowing($followerId,$followTargetId){
+
+    	$temp = DB::table('followings')->where('userId',$followerId)->where('followingId',$followTargetId)->get();
+    	if($temp->isEmpty()){
+    		return false;
+    	}else{
+    		return true;
+    	}
+    	
+    }
+
     public function index($id){
     	$user = User::where('id',$id)->first();
-    	return view('Profile.index')->with(['user'=>$user]);
+    	$currentUser = Auth::user()->id;
+    	$isFollowing = $this->checkFollowing($currentUser,$id);
+
+    	return view('Profile.index')->with(['user'=>$user,'isFollowing'=>$isFollowing]);
     }
 
     public function follow(Request $request){
     	//dd($request);
     	$followerId = $request->followerID;
     	$followTargetId = $request->followTargetID;   	
-    	$temp = DB::table('followings')->where('userId',$followerId)->where('followingId',$followTargetId)->get();
-
-    	if($temp->isEmpty()){
+    	
+    	if($this->checkFollowing($followerId,$followTargetId)){
+    		DB::table('followings')->where('userId',$followerId)->where('followingId',$followTargetId)->delete();
+    		return "Follow";
+    	}else{
     		DB::table('followings')->insert([
                 'userId' => $followerId,
                 'followingId'=>$followTargetId,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
               ]);
-    	}else{
-    		$temp->delete();
-    		return "Follow";
+    		return "Following";
     	}
-
-    	return "Followed";
     }
+
+    
+
 }
